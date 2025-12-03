1af3c4fbe3f3689bfe4a0f5fdb96c4d6e829c8d8
Jovanie Felamin <kkangpae59@gmail.com>
2025-12-04 06:23:38 +0800

    removed imagekit

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com"],
     remotePatterns: [
-      { protocol: "https", hostname: "ik.imagekit.io" },
       { protocol: "https", hostname: "syukwbfglfsmhzziekls.supabase.co" },
+      { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_PUBLIC_KEY: process.env.NEXT_PUBLIC_PUBLIC_KEY,
-    PRIVATE_KEY: process.env.PRIVATE_KEY,
-    NEXT_PUBLIC_URL_ENDPOINT: process.env.NEXT_PUBLIC_URL_ENDPOINT,
-  },
 };
 
 export default nextConfig;
